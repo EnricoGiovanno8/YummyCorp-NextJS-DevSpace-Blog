e033b368704fa15fcf97d4edf94b1fa2fedc1689
EnricoGiovanno8 <genrico189@gmail.com>
2022-01-11 15:34:03 +0700

    Sort Posts By Date Done

--- pages/index.js
+++ pages/index.js
@@ -4,6 +4,7 @@ import matter from 'gray-matter'
 import Layout from "../components/Layout";
 import Link from 'next/link';
 import Post from '../components/Post';
+import { sortByDate } from '../utils'
 
 export default function HomePage({ posts }) {
   return (
@@ -51,7 +52,7 @@ export async function getStaticProps() {
 
   return {
     props: {
-      posts
+      posts: posts.sort(sortByDate).slice(0,6)
     }
   }
 }
