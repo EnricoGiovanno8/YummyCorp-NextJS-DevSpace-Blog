6b02720d2ff40f1fc1fc91ed89d101127184bd75
EnricoGiovanno8 <genrico189@gmail.com>
2022-01-12 14:54:48 +0700

    Search Results Component Done

--- components/Post.js
+++ components/Post.js
@@ -3,7 +3,7 @@ import Link from "next/link"
 import { useRouter } from "next/router"
 import CategoryLabel from "./CategoryLabel"
 
-export default function Post({ post }) {
+export default function Post({ post, compact }) {
     const router = useRouter()
 
     const blogPage = () => {
@@ -12,7 +12,7 @@ export default function Post({ post }) {
 
     return (
         <div className="w-full px-10 py-6 bg-white rounded-lg shadow-md mt-6">
-            <Image src={post.frontmatter.cover_image} alt="" height={420} width={600} className="mb-4 rounded" />
+            {!compact && (<Image src={post.frontmatter.cover_image} alt="" height={420} width={600} className="mb-4 rounded" />)}
 
             <div className="flex justify-between items-center">
                 <span className="font-light text-gray-600">
@@ -30,18 +30,21 @@ export default function Post({ post }) {
                 <p className="mt-2 text-gray-600">{post.frontmatter.excerpt}</p>
             </div>
 
-            <div className="flex justify-between items-center mt-6">
-                <Link href='#'>
-                    <a className="text-gray-900 hover:text-blue-600" onClick={blogPage}>
-                        Read More
-                    </a>
-                </Link>
-                <div className="flex items-center">
-                    {/* eslint-disable-next-line @next/next/no-img-element */}
-                    <img src={post.frontmatter.author_image} alt="" className="mx-4 w-10 h-10 object-cover rounded-full hidden sm:block"/>
-                    <h5 className="text-gray-700 font-bold">{post.frontmatter.author}</h5>
+            {!compact && (
+                <div className="flex justify-between items-center mt-6">
+                    <Link href='#'>
+                        <a className="text-gray-900 hover:text-blue-600" onClick={blogPage}>
+                            Read More
+                        </a>
+                    </Link>
+                    <div className="flex items-center">
+                        {/* eslint-disable-next-line @next/next/no-img-element */}
+                        <img src={post.frontmatter.author_image} alt="" className="mx-4 w-10 h-10 object-cover rounded-full hidden sm:block"/>
+                        <h5 className="text-gray-700 font-bold">{post.frontmatter.author}</h5>
+                    </div>
                 </div>
-            </div>
+            )}
+            
         </div>
     )
 }
