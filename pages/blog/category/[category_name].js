4c4e85640b71831ea28c5ca71f4270c428b8ffc1
EnricoGiovanno8 <genrico189@gmail.com>
2022-01-12 11:10:13 +0700

    Clean up Get Posts Done

--- pages/blog/category/[category_name].js
+++ pages/blog/category/[category_name].js
@@ -2,9 +2,8 @@ import fs from 'fs'
 import path from 'path'
 import matter from 'gray-matter'
 import Layout from "@/components/Layout";
-import Link from 'next/link';
 import Post from '@/components/Post';
-import { sortByDate } from '@/utils/index'
+import { getPosts } from '@/lib/posts';
 
 export default function CategoryBlogPage({ posts, categoryName }) {
   return (
@@ -44,28 +43,14 @@ export async function getStaticPaths() {
 export async function getStaticProps({ params: { category_name }}) {
     const files = fs.readdirSync(path.join('posts'))
 
-    const posts = files.map((filename) => {
-        const slug = filename.replace('.md', '')
-
-        const markdownWithMeta = fs.readFileSync(
-            path.join('posts', filename),
-            'utf-8'
-        )
-
-        const { data: frontmatter } = matter(markdownWithMeta)
-
-        return {
-                slug,
-                frontmatter
-            }
-    })
+    const posts = getPosts()
 
     // Filter posts by category
     const filteredPosts = posts.filter((post) => post.frontmatter.category.toLowerCase() === category_name)
 
     return {
         props: {
-            posts: filteredPosts.sort(sortByDate),
+            posts: filteredPosts,
             categoryName: category_name
         }
     }
--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,13 @@ module.exports = {
     "./pages/**/*.{js,ts,jsx,tsx}",
     "./components/**/*.{js,ts,jsx,tsx}",
   ],
+  safelist: [
+    'bg-yellow-600',
+    'bg-blue-600',
+    'bg-green-600',
+    'bg-purple-600',
+    'bg-red-600',
+  ],
   theme: {
     extend: {},
   },
